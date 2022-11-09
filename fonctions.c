b2a7ae4eaf66b7987fcebb3d86e16fab007eb26f
AsiaGrpr <groupierre.asia@gmail.com>
2022-11-09 11:20:26 -0800

    fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -40,7 +40,7 @@ int print_string(va_list all)
 {
 	char *s = va_arg(all, char *);
 
-  	if (s == NULL)
+	if (s == NULL)
 		return (0);
 
 	return (write(1, s, _strlen(s)));
@@ -49,12 +49,13 @@ int print_string(va_list all)
 /**
  * print_percent - function that stdout % symbol
  * @all: va_list used as input
- * 
+ *
  * Return: Length value
 */
 int print_percent(va_list all)
 {
 	int c = va_arg(all, int);
+	
 	c = 37;
 
 	write(1, &c, 1);
