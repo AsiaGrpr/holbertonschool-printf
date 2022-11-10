1cc7b2ac210d5d8dd6c7c9818e81454b6f3982de
AsiaGrpr <groupierre.asia@gmail.com>
2022-11-10 01:05:59 -0800

    print_string correction

--- fonctions.c
+++ fonctions.c
@@ -39,22 +39,20 @@ int print_string(va_list all)
 {
 	char *s = va_arg(all, char *);
 
-	if (s == NULL)
-		return (0);
-
+  	s == NULL ? s = "(Null)" : s = s;
 	return (write(1, s, _strlen(s)));
 }
 
 /**
  * print_percent - function that stdout % symbol
  * @all: va_list used as input
- *
+ * 
  * Return: Length value
 */
 int print_percent(va_list all)
 {
 	int c = va_arg(all, int);
-
 	c = 37;
+
 	return (write(1, &c, 1));
 }
